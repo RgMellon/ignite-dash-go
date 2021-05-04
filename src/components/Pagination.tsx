fecbc37123afd22a42c63b2751e6976fba315de0
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2021-05-04 18:49:15 -0300

    add user list

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,63 @@
+import { Stack, Button, Box } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
+      <Box>
+        <strong>0</strong> - <strong> 10 </strong> de <strong>100</strong>
+      </Box>
+
+      <Stack direction="row" spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{
+            cursor: "default",
+            bgColor: "pink.500",
+          }}
+        >
+          1
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bgColor="gray.700"
+          _hover={{ bgColor: "gray.500" }}
+        >
+          3
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bgColor="gray.700"
+          _hover={{ bgColor: "gray.500" }}
+        >
+          4
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bgColor="gray.700"
+          _hover={{ bgColor: "gray.500" }}
+        >
+          5
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
